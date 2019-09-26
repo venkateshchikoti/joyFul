54f48a8d0390edc75ef2dd647ec5a3c6d3c51876
Venkatesh.Chikoti <45027880+vchikoti@users.noreply.github.com>
2019-09-26 15:57:51 +0530

    new code

---
+++ src/components/Faq/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FaqItem from "./../FaqItem";
+import "./styles.scss";
+
+function Faq(props) {
+  return (
+    <>
+      {props.items.map((item, index) => (
+        <FaqItem question={item.question} answer={item.answer} key={index} />
+      ))}
+    </>
+  );
+}
+
+export default Faq;
